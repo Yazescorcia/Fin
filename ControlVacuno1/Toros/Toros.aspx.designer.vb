65ea568cb24f138364ed6127e9377ee0188e9012
Yazmin <yazescorcia091201@gmail.com>
2022-03-15 15:08:27 -0600

    Segundo

---
+++ ControlVacuno1/Toros/Toros.aspx.designer.vb
@@ -0,0 +1,150 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Toros
+
+    '''<summary>
+    '''Control lblArete.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblArete As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtArete.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtArete As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control lblNombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblNombre As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtNombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtNombre As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control lbtFecha.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbtFecha As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtFecha.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtFecha As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control lblProceso.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblProceso As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtProceso.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtProceso As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtPorcentaje.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtPorcentaje As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control btnInsertar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnInsertar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnModificar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnModificar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnEliminar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnEliminar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control gvToros.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents gvToros As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control SqlDataSourceToros.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSourceToros As Global.System.Web.UI.WebControls.SqlDataSource
+End Class
---
+++ ControlVacuno1/Toros/Toros.aspx.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class Toros
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+        Try
+            Dim sqL As String = "INSERT INTO [dbo].[Toro]([arete],[nombre],[fecha],[proceso],[porcentaje]) VALUES ('" + txtArete.Text + "','" + txtNombre.Text + "','" + txtFecha.Text + "','" + txtProceso.Text + "','" + txtPorcentaje.Text + "')"
+            Dim reader As SqlDataReader
+            Dim conexion As New SqlConnection(get_ConnectionString)
+            conexion.Open()
+            Dim miComandoSql As New SqlCommand
+            miComandoSql.CommandText = sqL
+            miComandoSql.Connection = conexion
+            reader = miComandoSql.ExecuteReader
+            conexion.Close()
+            txtArete.Text = ""
+            txtNombre.Text = ""
+            txtFecha.Text = ""
+            txtProceso.Text = ""
+            txtPorcentaje.Text = ""
+            gvToros.DataBind()
+            SqlDataSourceToros.DataBind()
+        Catch ex As Exception
+            Response.Write(ex.ToString)
+        End Try
+    End Sub
+    Public Function get_ConnectionString() As String
+        Dim sql_conexion_string As String = "Data Source=DESKTOP-K3KI45J\SQLEXPRESS; Initial Catalog=Ganado; User ID=sa; Password=yyy"
+        Return sql_conexion_string
+    End Function
+
+    Protected Sub gvToros_SelectedIndexChanged(sender As Object, e As EventArgs) Handles gvToros.SelectedIndexChanged
+        txtArete.Text = gvToros.SelectedRow.Cells(1).Text.ToString
+        txtNombre.Text = gvToros.SelectedRow.Cells(2).Text.ToString
+        txtFecha.Text = gvToros.SelectedRow.Cells(3).Text.ToString
+        txtProceso.Text = gvToros.SelectedRow.Cells(4).Text.ToString
+        txtPorcentaje.Text = gvToros.SelectedRow.Cells(5).Text.ToString
+    End Sub
+
+    Protected Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Try
+            Dim sqL As String = "UPDATE [dbo].[Toro] SET [nombre] = '" + txtNombre.Text + "',[fecha] = '" + txtFecha.Text + "',[padre] = '" + txtProceso.Text + "',[madre] = '" + txtPorcentaje.Text + "' WHERE [arete]='" + txtArete.Text + "'"
+            Dim reader As SqlDataReader
+            Dim conexion As New SqlConnection(get_ConnectionString)
+            conexion.Open()
+            Dim miComandoSql As New SqlCommand
+            miComandoSql.CommandText = sqL
+            miComandoSql.Connection = conexion
+            reader = miComandoSql.ExecuteReader
+            conexion.Close()
+            txtArete.Text = ""
+            txtNombre.Text = ""
+            txtFecha.Text = ""
+            txtProceso.Text = ""
+            txtPorcentaje.Text = ""
+            gvToros.DataBind()
+            SqlDataSourceToros.DataBind()
+        Catch ex As Exception
+            Response.Write(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Try
+            Dim sql As String = "DELETE FROM [dbo].[Toro] WHERE [arete] = '" + txtArete.Text + "'"
+            Dim reader As SqlDataReader
+            Dim conexion As New SqlConnection(get_ConnectionString)
+            conexion.Open()
+            Dim miComandoSql As New SqlCommand
+            miComandoSql.CommandText = sql
+            miComandoSql.Connection = conexion
+            reader = miComandoSql.ExecuteReader
+            conexion.Close()
+            txtArete.Text = ""
+            txtNombre.Text = ""
+            txtFecha.Text = ""
+            txtProceso.Text = ""
+            txtPorcentaje.Text = ""
+            gvToros.DataBind()
+            SqlDataSourceToros.DataBind()
+        Catch ex As Exception
+            Response.Write(ex.ToString)
+        End Try
+    End Sub
+End Class
